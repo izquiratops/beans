f27e238fff3e915dec5ea7ff8a5eb4940b580f93
Jordi Izquierdo <hi@izquiratops.dev>
2022-06-22 09:06:31 +0200

    /run command

--- src/modules/beans.py
+++ src/modules/beans.py
@@ -2,15 +2,15 @@ import os
 import openai
 
 from telegram import Update
-from telegram.ext import MessageHandler, CommandHandler, CallbackContext, filters
+from telegram.ext import CommandHandler, CallbackContext, filters
 
 from src.modules.whitelist import Whitelisted
 
 
-async def message_callback(update: Update, _: CallbackContext) -> None:
+async def run_callback(update: Update, _: CallbackContext) -> None:
     response = openai.Completion.create(
         model="text-davinci-002",
-        prompt=update.message.text,
+        prompt=update.message.text[5:],
         temperature=0,
         max_tokens=1024)
 
@@ -27,13 +27,14 @@ class Beans:
 
     def __init__(self, application) -> None:
         openai.api_key = os.getenv("OPENAI_API_KEY")
+        message_filter = Whitelisted()
 
         # Handlers
         whoami = CommandHandler("whoami", whoami_callback)
-
-        message_filters = Whitelisted() & filters.TEXT
-        message = MessageHandler(message_filters, message_callback)
+        run = CommandHandler("run", run_callback, message_filter)
+        # TODO: message = MessageHandler(message_filters, message_callback)
 
         # Dispatcher
         application.add_handler(whoami)
-        application.add_handler(message)
+        application.add_handler(run)
+        # TODO: application.add_handler(message)
